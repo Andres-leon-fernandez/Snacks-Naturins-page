7d2fd40222de0c0c9f52151c416d65eb1cbb2ff5
Andrés León <andres05leonfernandez@gmail.com>
2026-04-07 11:51:58 -0500

    refactor: convert footer into reusable component

---
+++ js/loadComponent.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("footer", "/js/component/footer.html");
+});
+
+function loadComponent(id, path) {
+  fetch(path)
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch((err) => console.error("Error al cargar el componente: ", id, err));
+}
